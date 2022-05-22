5d209b1937f115f8a3257ca243fa107addfd4678
smileqoo <105653195+smileqoo@users.noreply.github.com>
2022-05-22 19:33:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,19 +7,15 @@ from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
+app = Flask(__name__)
+
+
 chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless") #無頭模式
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-
-
-
-
-
-
-app = Flask(__name__)
 
 line_bot_api = LineBotApi(os.environ.get("CHANNEL_ACCESS_TOKEN"))
 handler = WebhookHandler(os.environ.get("CHANNEL_SECRET"))
@@ -29,8 +25,8 @@ handler = WebhookHandler(os.environ.get("CHANNEL_SECRET"))
 def callback():
 
     if request.method == "GET":
-        driver.get("https://www.google.com")
-        print(driver.page_source)
+        #driver.get("https://www.google.com")
+        #print(driver.page_source)
         return "Hello Heroku"
     if request.method == "POST":
         signature = request.headers["X-Line-Signature"]
