d17025c9c35f6040861a2e34f656bbb2c764da3e
smileqoo <105653195+smileqoo@users.noreply.github.com>
2022-05-22 19:19:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 import os
 from selenium import webdriver
+from datetime import datetime
+
+from flask import Flask, abort, request
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
@@ -8,5 +14,40 @@ chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
-driver.get("https://www.google.com")
-print(driver.page_source)
+
+
+
+
+
+app = Flask(__name__)
+
+line_bot_api = LineBotApi(os.environ.get("CHANNEL_ACCESS_TOKEN"))
+handler = WebhookHandler(os.environ.get("CHANNEL_SECRET"))
+
+
+@app.route("/", methods=["GET", "POST"])
+def callback():
+
+    if request.method == "GET":
+        driver.get("https://www.google.com")
+        print(driver.page_source)
+        return "Hello Heroku"
+    if request.method == "POST":
+        signature = request.headers["X-Line-Signature"]
+        body = request.get_data(as_text=True)
+        
+        try:
+            handler.handle(body, signature)
+        except InvalidSignatureError:
+            abort(400)
+
+        return "OK"
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    get_message = event.message.text
+
+    # Send To Line
+    reply = TextSendMessage(text=f"{get_message}")
+    line_bot_api.reply_message(event.reply_token, reply)
